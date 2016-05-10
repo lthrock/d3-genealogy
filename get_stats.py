80bac715f42ca68e05a3cdf10a9781f3adfcdd9f
Lucas T <lucast@stanford.edu>
2016-05-09 23:50:12 -0700

    rewrote moss scrape code

--- get_stats.py
+++ get_stats.py
@@ -2,62 +2,100 @@ import urllib
 from bs4 import BeautifulSoup
 import re
 import csv
+import sys
+
+idNumber = '781444417'
 
 # CSV output file
-primary_output = open("main_moss_output.csv", 'wb')
-detail_output = open("detail_moss_output.csv", 'wb')
+primary_output = open('main_moss_output_' + idNumber + '.csv', 'wb')
+detail_output = open('detail_moss_output_' + idNumber + '.csv', 'wb')
 wr1 = csv.writer(primary_output, quoting=csv.QUOTE_ALL)
 wr2 = csv.writer(detail_output, quoting=csv.QUOTE_ALL)
 
+wr1.writerow(['Detail Url', 'File 1', 'Confidence', 'File 2', 'Confidence', 'Number of Matched Lines'])
+wr2.writerow(['File 1', 'Confidence', 'Start Line', 'End Line', 'File 2', 'Confidence', 'Start Line', 'End Line'])
+
 # Fetch moss results, b-e-a-utify
 url = "http://moss.stanford.edu/results/781444417/"
-f = urllib.urlopen(url)
-soup = BeautifulSoup(f.read(), "html.parser")
-
-# Regex for parsing rows in ouptut
-# p = re.compile('([a-zA-Z_0-9.]*) \((\d+)%\)')
-# Forced to use excessive regex due to malformed html (no </tr> tags...)
-p = re.compile('<tr><td><a href="(http://moss.stanford.edu/results/.*.html)">[0-9a-zA-Z.]+ \((\d+)%\)</a>\n<td><a href="http://moss.stanford.edu/results/.*.html">[0-9a-zA-Z.]+ \((\d+)%\)</a>\n<td align="right">(\d+).*')
+htmlContents = urllib.urlopen(url).read()
+soup = BeautifulSoup(htmlContents, 'html.parser')
 
 # to construct links for detailed info on all comparisons reported
 comparisonCount =  len(soup.findAll('tr')) - 1
 
-# convert page contents to csv format, output to file
-for tr in soup.findAll('tr'):
-	m = p.match(str(tr))
-	if m:
-		row = []
-		for i in range(1, 5):
-			row.append(m.group(i))
+# skip first row
+rowStartIndex = htmlContents.find('<TR>')
+rowEndIndex = htmlContents.find('<TR>', rowStartIndex + 1)
+
+while (rowEndIndex != -1):
+	rowStartIndex = rowEndIndex
+	rowEndIndex = htmlContents.find('<TR>', rowStartIndex + 1)
+	rowSoup = BeautifulSoup(htmlContents[rowStartIndex : rowEndIndex], 'html.parser')
+
+	aTags = rowSoup.findAll('a')
+	if len(aTags) > 0:
+		row = [aTags[0]['href']]
+		for a in aTags:
+			contents = a.contents[0]
+
+			addressEndIndex = contents.find('.html') + 5
+			address = contents[ : addressEndIndex]
+			percentEndIndex = contents.find('%', addressEndIndex)
+			percentMatch = contents[addressEndIndex + 2 : percentEndIndex]
+
+			numberOfLines = rowSoup.findAll('td')[2].contents[0].strip()
+
+			row.append(address)
+			row.append(percentMatch)
+		
+		row.append(numberOfLines)	
 		wr1.writerow(row)
 
-p = re.compile('[.\n]*<th>([0-9a-zA-Z]*.html) \((\d+)%\).*<th>([0-9a-zA-Z]*.html) \((\d+)%\).*')
+
+# Detail pages:
 for i in range(0, comparisonCount):
 	# Get new page's html
 	newUrl = url + "match" + str(i) + "-top.html"
-	f = urllib.urlopen(newUrl)
-	soup = BeautifulSoup(f.read(), "html.parser")
+	htmlContents = urllib.urlopen(newUrl).read()
+	soup = BeautifulSoup(htmlContents, "html.parser")
 
-	# add data to csv
-	m = p.match(str(soup.find('th')))
-	name1 = 'error'
-	percent1 = 'error'
-	name2 = 'error'
-	percent2 = 'error'
-	if m:
-		name1 = m.group(1)
-		percent1 = m.group(2)
-		name2 = m.group(3)
-		percent2 = m.group(4)
 
-	counter = 0
-	row = []
-	for a in soup.findAll('a'):
-		if counter % 2 == 0:
-			lineRange = str(a.contents[0])
-			if counter % 4 == 0:
-				row = [name1, percent1] + lineRange.split('-')
-			else:
-				row += [name2, percent2] + lineRange.split('-')
-				wr2.writerow(row)
-		counter += 1
+	# skip first row
+	rowStartIndex = htmlContents.find('<TR>')
+	rowEndIndex = htmlContents.find('<TR>', rowStartIndex + 1)
+
+	rowSoup = BeautifulSoup(htmlContents[rowStartIndex : rowEndIndex], 'html.parser')
+	rowContents = str(rowSoup) 
+
+	file1StartIndex = 8
+	file1EndIndex = rowContents.find('.html', file1StartIndex) + 5
+	file2StartIndex = rowContents.find('.gif"><th>') + 10
+	file2EndIndex = rowContents.find('.html', file2StartIndex) + 5
+
+	file1 = rowContents[file1StartIndex : file1EndIndex]
+	file2 = rowContents[file2StartIndex : file2EndIndex]
+
+	percent1StartIndex = rowContents.find('(', file1EndIndex) + 1
+	percent1EndIndex = rowContents.find('%', percent1StartIndex)
+	percent2StartIndex = rowContents.find('(', file2EndIndex) + 1
+	percent2EndIndex = rowContents.find('%', percent2StartIndex)
+
+	percent1 = rowContents[percent1StartIndex : percent1EndIndex]
+	percent2 = rowContents[percent2StartIndex : percent2EndIndex]
+
+	while rowEndIndex != -1:
+		rowStartIndex = rowEndIndex
+		rowEndIndex = htmlContents.find('<TR>', rowStartIndex+1)
+		rowSoup = BeautifulSoup(htmlContents[rowStartIndex : rowEndIndex], 'html.parser')
+
+		counter = 0
+		row = []
+		for a in soup.findAll('a'):
+			if counter % 2 == 0:
+				lineRange = str(a.contents[0])
+				if counter % 4 == 0:
+					row = [file1, percent1] + lineRange.split('-')
+				else:
+					row += [file2, percent2] + lineRange.split('-')
+					wr2.writerow(row)
+			counter += 1
