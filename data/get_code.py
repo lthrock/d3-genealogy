403f321b7c9680cda785e12823e506fa217cb1ce
Cathy Zhu <cathyxz@gmail.com>
2016-05-09 19:55:29 -0700

    added data

---
+++ data/get_code.py
@@ -0,0 +1,25 @@
+import json
+
+
+with open('mbostock.json') as json_data: 
+  d=json.load(json_data)
+  json_data.close()
+
+code_array = d["hits"]["hits"]
+
+for element in code_array:
+  gistid = element["_id"]
+  e = element["_source"]
+  code = e["code"].encode('ascii', 'ignore')
+  author = e["userId"]
+  
+  filename = author + '_' + gistid + '.html'
+  outfile = open(filename, 'w')
+  outfile.write(code)
+  
+#  print e["created_at"]
+#  print e["updated_at"]
+#  print e["api"]
+#  print e["readme"]
+#  print e["description"]
+
