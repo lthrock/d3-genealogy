138b8c4f689cfaf80046343a3ad6d9b8c931fe3e
Cathy Zhu <cathyxz@gmail.com>
2016-05-09 21:40:41 -0700

    take input file as arg

--- data/get_code.py
+++ data/get_code.py
@@ -1,21 +1,38 @@
-import json
+#!/usr/bin/python
 
+import json, sys, getopt
 
-with open('force-layout.json') as json_data: 
-  d=json.load(json_data)
-  json_data.close()
+# Usage: ./get_code.py -i <inputfile>
 
-code_array = d["hits"]["hits"]
+def main(argv):
+  inputfile=''
+  try:
+    opts, args = getopt.getopt(argv,"h:i:",["ifile="])
+  except getopt.GetoptError:
+    print 'Usage: test.py -i <inputfile>'
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+       print 'test.py -i <inputfile>'
+       sys.exit()
+    elif opt in ("-i", "--ifile"):
+       inputfile = arg
+  print inputfile
+  with open(inputfile) as json_data: 
+    d=json.load(json_data)
+    json_data.close()
 
-for element in code_array:
-  gistid = element["_id"]
-  e = element["_source"]
-  code = e["code"].encode('ascii', 'ignore')
-  author = e["userId"]
-  
-  filename = author + '_' + gistid + '.html'
-  outfile = open(filename, 'w')
-  outfile.write(code)
+  code_array = d["hits"]["hits"]
+
+  for element in code_array:
+    gistid = element["_id"]
+    e = element["_source"]
+    code = e["code"].encode('ascii', 'ignore')
+    author = e["userId"]
+
+    filename = author + '_' + gistid + '.html'
+    outfile = open(filename, 'w')
+    outfile.write(code)
   
 #  print e["created_at"]
 #  print e["updated_at"]
@@ -23,3 +40,5 @@ for element in code_array:
 #  print e["readme"]
 #  print e["description"]
 
+if __name__ == "__main__":
+   main(sys.argv[1:])
