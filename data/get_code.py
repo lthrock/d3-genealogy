b83cfb6c50d7fb4efa71bd96de3196b849d7b5a7
Cathy Zhu <cathyxz@gmail.com>
2016-05-21 13:02:56 -0700

    added parse_moss script to generate links information, new data in force-layout.json

--- data/get_code.py
+++ data/get_code.py
@@ -4,24 +4,12 @@ import json, sys, getopt, re
 
 # Usage: ./get_code.py -i <inputfile>
 
-def main(argv):
-  inputfile=''
-  try:
-    opts, args = getopt.getopt(argv,"h:i:",["ifile="])
-  except getopt.GetoptError:
-    print 'Usage: test.py -i <inputfile>'
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-       print 'test.py -i <inputfile>'
-       sys.exit()
-    elif opt in ("-i", "--ifile"):
-       inputfile = arg
-  print inputfile
+def main(argv): 
+  inputfile = argv[0]
+  
   with open(inputfile) as json_data: 
     d=json.load(json_data)
     json_data.close()
-
   code_array = d["hits"]["hits"]
   
   output_json = []
@@ -38,6 +26,7 @@ def main(argv):
       outfile = open(filename, 'w')
       outfile.write(code)
       simple_e = {}
+      simple_e["uid"] = author + '_' + gistid
       simple_e["created_at"] = e["created_at"]
       simple_e["updated_at"] = e["updated_at"]
       simple_e["api"] = e["api"]
@@ -47,8 +36,8 @@ def main(argv):
     
     
   print len(output_json)
-  with open('data.json', 'w') as datafile:
-    json.dump(output_json, datafile)
+  with open('nodes.json', 'w') as datafile:
+    json.dump(output_json, datafile)   
     
 def get_js_only(code):
   re.DOTALL
---
+++ data/parse_moss.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import json, sys, getopt, re, csv, os
+
+def main(argv):
+  mossfile = argv[0]
+  outfile = "links.json"
+  links = []
+  with open(mossfile) as moss_data: 
+    moss_reader = csv.reader(moss_data, delimiter=',')
+    headers = next(moss_reader, None)
+    for row in moss_reader:
+      # Only create links with more than 10 lines matched
+      if(int(row[5]) > 10): 
+        # Get file_id
+        file1_id = os.path.splitext(os.path.split(row[1])[1])[0]
+        file1_data = file1_id.split('_')
+        file1_author = file1_data[0]
+        file2_id = os.path.splitext(os.path.split(row[3])[1])[0]
+        file2_data = file2_id.split('_')
+        file2_author = file2_data[0]
+
+        if(file1_author <> file2_author):
+          print "NOT SAME"
+          e = {}
+          e["source"] = file1_id
+          e["target"] = file2_id
+          # lines matched
+          e["value"] = int(row[5])
+          links.append(e)
+      
+    print len(links)
+    with open('links.json', 'w') as linksfile:
+      json.dump(links, linksfile)  
+if __name__ == "__main__":
+   main(sys.argv[1:])
