4b51dcb1b9298d7bd5040152bb0579b658ecb604
Cathy Zhu <cathyxz@gmail.com>
2016-06-02 17:16:20 -0700

    filter to direct parent links only

--- data/filter_nodes.py
+++ data/filter_nodes.py
@@ -2,7 +2,6 @@
 
 import json, sys, getopt, re
 
-#def main(argv):
 # Open files and import json
 linkfile = 'links.json'
 with open(linkfile) as links_data: 
@@ -17,8 +16,28 @@ with open(nodefile) as nodes_data:
 node_ids = [e["source"] for e in links]
 node_ids += [e["target"] for e in links]
 nodes = [i for i in nodes if i["uid"] in node_ids]
-print len(nodes)
 
-cleanedfile = 'nodes-clean.json'
-with open(cleanedfile, 'w') as clean_file:
-  json.dump(nodes, clean_file)
+
+# Filter links to direct parents only
+parents = []
+for node in nodes:
+  ancestor_links = [link for link in links if link["target"] == node["uid"]]
+  if(len(ancestor_links) > 0): 
+    ancestors = []
+    for link in ancestor_links: 
+      ancestor = next((x for x in nodes if x["uid"] == link["source"]), None)
+      ancestors.append(ancestor)
+      
+    ancestors = sorted(ancestors, key=lambda x: x["created_at"], reverse=True)
+    parent_node = ancestors[0]
+    parent_link = next(x for x in ancestor_links if x["source"] == parent_node["uid"])
+    parents.append(parent_link)
+
+# Generate output links
+data = {}
+data["nodes"] = nodes
+data["links"] = parents
+
+outfilename = 'd3-mouse-output.json'
+with open(outfilename, 'w') as outfile: 
+  json.dump(data, outfile)
